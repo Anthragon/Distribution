0c8aadce5333c33dc6966c2639ad3c1b9a0d2f2f
lumi2021 <94462076+lumi2021@users.noreply.github.com>
2025-07-08 22:47:45 -0300

    Submodules: Update dependencies

--- afterrun.sh
+++ afterrun.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BIN="./zig-out/disk/kernel"
+BIN="./zig-out/disk/boot/kernel"
 INPUT_LOG="./zig-out/stderr.txt"
 VMA_BASE="0xFFFFFFFF80000000"
 
@@ -18,7 +18,7 @@ while IFS= read -r line; do
     elif [[ "$line" == "<===addr===/>" ]]; then
         in_section=false
         while IFS= read -r addr; do
-            location="0x$addr: $(addr2line -e "$BIN" -f -C -i -p "$addr")"
+            location="$addr: $(addr2line -e "$BIN" "0x$addr")"
             echo "$location" >> "$TMP_FILE"
         done < "$TMP_ADDRS"
         continue
