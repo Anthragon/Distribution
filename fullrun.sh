964604a923c59566a4d7d0fd29dbcee66cd42232
Camila <94462076+lumi2021@users.noreply.github.com>
2025-09-12 15:20:03 -0300

    Submodules: Update dependencies

--- fullrun.sh
+++ fullrun.sh
@@ -1,5 +1,5 @@
 zig build run \
 -DbiosMode=bios \
 -DdiskLayout=GPT \
--DsystemUsers=camila,fa5c7724-18d8-4d21-a782-9732b4e5c028,P \
+-DsystemUsers=camila,fa5c7724-18d8-4d21-a782-9732b4e5c028,A \
 -freference-trace
